8696b0bac94beea9a2d4b2149efae1848b202443
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2020-11-26 00:21:59 -0800

    gitignore apikeys.py update

--- Output/api_keys.py
+++ Output/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ee80bb1d5b637b8ed3144ed4eb70d8a3"
+weather_api_key = "Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyClVkgz0YXI-ZEO3yz3z85eqxEFmyakVTU"
+g_key = "Your Key Here"
